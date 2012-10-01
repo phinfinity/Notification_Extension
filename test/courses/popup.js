d342646fe2e13e2c92fa6a5175cfc775c0f6b68a
Anish Shankar <rndanish@gmail.com>
2012-10-02 01:00:13 +0530

    Lots of UI-reworks . Added Image, Re-aligned, Link Support

--- test/courses/popup.js
+++ test/courses/popup.js
@@ -16,7 +16,33 @@ content = document.getElementById("content");
 var retstr="<hr/>"
 for (i=0;i<vals.length;i++) {
 	var notification_string = summarize_text($($("font",vals[i])[0]).text());
-	retstr += "<div class='notification_entry'>"+notification_string+"</div><hr/>";
+	var notification_title = "";
+	var notification_image = 'http://courses.iiit.ac.in/EdgeNet/' + $("img",vals[i])[1].attributes.getNamedItem('src').textContent;
+	var notification_link = 'http://courses.iiit.ac.in/EdgeNet/' + $('a',vals[i])[2].attributes.getNamedItem('href').textContent;
+	// Set above variables depending on context and leave below unchanged
+	if (notification_title != "")
+		notification_title += "- ";
+	//Sanitize strings
+	notification_string = $("<div/>").text(notification_string).html();
+	notification_title = $("<div/>").text(notification_title).html();
+	notification_image = $("<div/>").text(notification_image).html();
+	notification_link = $("<div/>").text(notification_link).html();
+	retstr += "<div class='notification_entry'><table><tr><td><img class='notification_image' src='"+notification_image+"'/></td><td><b>"+notification_title+"</b>"+notification_string+"</td></tr></table><div class='notification_link'>"+notification_link+"</div></div><hr/>";
 }
 retstr += "";
 document.getElementById("content").innerHTML=retstr;
+$(".notification_entry").click(function(){
+	chrome.tabs.create({
+		url: $(this).find(".notification_link").text(),
+		active: false
+	});
+});
+
+$(".notification_entry").hover(
+		function() {
+			$(this).css("background-color","#E0E0E0");
+		},
+		function() {
+			$(this).css("background-color","#FFFFFF");
+		});
+
--- test/courses/popupstyles.js
+++
@@ -1,8 +0,0 @@
-$(".notification_entry").hover(
-		function() {
-			$(this).css("background-color","#E0E0E0");
-		},
-		function() {
-			$(this).css("background-color","#FFFFFF");
-		});
-
