9554b62fa9ba502a3ff358edfaa0b9d631061cb8
invader <sanjay.slnarayanan@gmail.com>
2012-10-02 13:27:06 +0530

    Added Refresh if time>24 hrs)

--- src/init.js
+++ src/init.js
@@ -1,8 +1,22 @@
 var TIME_TO_REFRESH=1000;
+var TIME_TO_CALL_REMHIST=86400
 function loop()
 {
        document.write(Math.round(new Date().getTime()/1000.0));
        //Runs Loop every TIME_TO_REFRESH Milliseconds   
-       setTimeout(init,TIME_TO_REFRESH);
+       setTimeout(loop,TIME_TO_REFRESH);
+}
+var last_init_time=localStorage.getItem("last_init_time");
+var curTime=(new Date().getTime()/1000.0);
+document.write(curTime);
+if(last_init_time==null)
+{
+	remHist();
+	localStorage.setItem("last_init_time",curTime.toString());
+}
+else if(curTime-parseInt(last_init_time)>=TIME_TO_CALL_REMHIST)
+{
+	remHist();
+	localStorage.setItem("last_init_time",curTime.toString());
 }
 loop();
