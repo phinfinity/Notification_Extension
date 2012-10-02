86d2c88970cc05de639965b2c72555c873d6fb9e
hashinclude <pulkitg10@gmail.com>
2012-10-02 12:22:35 +0530

    Descending Sort

--- src/background.js
+++ src/background.js
@@ -5,8 +5,8 @@ function sortfunc(a, b) {
 	if (a["time"] == b["time"])
 		return 0;
 	if (parseInt(a["time"]) > parseInt(b["time"]))
-		return 1;
-	return -1;
+		return -1;
+	return 1;
 
 }
 function addNotif(notifObj){
@@ -14,8 +14,9 @@ function addNotif(notifObj){
 	   new_notifications list and the old_notifications 
 	   list*/
 	curnew_notifs=JSON.parse(localStorage.getItem("new_notifications"));		//get string from localStorage
-	if(curnew_notifs==null)
-		curnew_notifs=[]
+	if(curnew_notifs==null){
+		curnew_notifs=[];
+	}
 	curnew_notifs.push(notifObj);
 	curnew_notifs.sort(sortfunc);
 
@@ -23,26 +24,27 @@ function addNotif(notifObj){
 	localStorage.setItem("new_notifications", JSON.stringify(curnew_notifs));
 	//updating localStorage
 	old_notifs = JSON.parse(localStorage.getItem("old_uids"));
-	if (old_notifs == null)
-		old_notifs = {}
+	if (old_notifs == null){
+		old_notifs = {};
+	}
 	old_notifs[notifObj.uid] = "{\"time\" : \"" + notifObj.time.toString() + "\" , \"dismissed\" : \"" + false + "\"}";
 	localStorage.setItem("old_uids", JSON.stringify(old_notifs))
 }
-
-/*function addNotif_test(){
- localStorage.clear();
- var obj1=JSON.parse("{\"uid\":\"UID1\",\"title\":\"Title1\",\"summary\":\"Summary1\",\"time\":\"123123123123\"}");
- var obj2=JSON.parse("{\"uid\":\"UID2\",\"title\":\"Title1\",\"summary\":\"Summary1\",\"time\":\"1\"}");
- addNotif(obj1);
- addNotif(obj2);
- console.log(localStorage.new_notifications)
- }
- */
+/*
+function addNotif_test(){
+	localStorage.clear();
+	var obj1=JSON.parse("{\"uid\":\"UID1\",\"title\":\"Title1\",\"summary\":\"Summary1\",\"time\":\"123123123123\"}");
+	var obj2=JSON.parse("{\"uid\":\"UID2\",\"title\":\"Title1\",\"summary\":\"Summary1\",\"time\":\"1\"}");
+	addNotif(obj2);
+	addNotif(obj1);
+	console.log(localStorage.new_notifications)
+}
+*/
 
 function addHist(uid, site) {
 	/* This function attempts to add uid,site after joining them
-	 It returns True on successful add, but returns false if
-	 Already exists and doesn't add */
+	   It returns True on successful add, but returns false if
+	   Already exists and doesn't add */
 	uidkey = uid + "#" + site;
 	histdb = JSON.parse(localStorage.getItem("old_uids"));
 	if (histdb == null) {
