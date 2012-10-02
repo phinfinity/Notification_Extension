0a3005acc4c9a4ad1a4e59e15cbf8cf548fb87ce
Anish Shankar <rndanish@gmail.com>
2012-10-02 11:56:08 +0530

    Added addHist()

--- src/background.js
+++ src/background.js
@@ -3,5 +3,22 @@
 
 
 function addHist(uid,site) {
-		
+	/* This function attempts to add uid,site after joining them
+	   It returns True on successful add, but returns false if
+	   Already exists and doesn't add */
+	  uidkey = uid+"#"+site;
+	  histdb = JSON.parse(localStorage.getItem("old_uids"));
+	  if (histdb == null) {
+	  	histdb = new Object();
+	  }
+	  if (histdb.hasOwnProperty(uidkey)) {
+	  	return false;
+	  } else {
+	  	var d = new Date();
+	  	var t = d.getTime();
+	  	histdb[uidkey] = {"time": t,"dismissed": true};
+	  	localStorage.setItem("old_uids", JSON.stringify(histdb));
+	  	return true;
+	  }
+
 }
