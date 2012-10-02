273300b7313140e8ba7dc635bb29769b9cd1a6d7
Anish Shankar <rndanish@gmail.com>
2012-10-02 19:00:05 +0530

    Coursese js

--- test/courses/courses.js
+++ test/courses/courses.js
@@ -1,28 +1,4 @@
 function get_notifications(site,callback) {
-	function parsedate(s){
-		var x=s.split(/\s{1,}/);
-		var yr=x[3].split(",")[1],dt=x[3].split(",")[0];
-		var hrs=parseInt(x[0].split(":")[0]),mins=x[0].split(":")[1];
-		if(x[1]=="pm"  && hrs <=12)
-			hrs+=12
-				var month=new Object()
-				month["Jan"]=1;
-		month["Feb"]=2;
-		month["Mar"]=3;
-		month["Apr"]=4;
-		month["May"]=5;
-		month["Jun"]=6;
-		month["Jul"]=7;
-		month["Aug"]=8;
-		month["Sep"]=9;
-		month["Oct"]=10;
-		month["Nov"]=11;
-		month["Dec"]=12;
-		var monthnum=month[x[2]].toString();
-		var retdate= new Date(yr,monthnum, dt,hrs.toString(),mins);
-		return retdate.getTime();
-
-	}
 	getpage("http://courses.iiit.ac.in/EdgeNet/home.php", function(t) {
 		doc = $(t);
 		vals = doc.find(".mainbox");
@@ -36,7 +12,8 @@ function get_notifications(site,callback) {
 			ob.uid = ob.summary;
 			var ht=$($("font",vals[i])[0]).html();
 			var date=ht.match(/<br>(.*)/)[1];
-			//ob.time = parsedate(date);
+			console.log(date);
+			ob.time = Date.parse(date);
 			ret.push(ob);
 		}
 		callback(ret);
