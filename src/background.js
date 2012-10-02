8c837b21ee7b0543f6029748e9ae74f19f79166a
Anish Shankar <rndanish@gmail.com>
2012-10-02 10:58:34 +0530

    Initial Commit for extension source

---
+++ src/background.js
@@ -0,0 +1,2 @@
+// This script will run on startup and will have  timer-loop running forever
+// Add background functions on top keep global constants on the very top of file
---
+++ src/foreground.js
@@ -0,0 +1,1 @@
+// Foreground helper functions this script will be loaded after jquery before popup.js
