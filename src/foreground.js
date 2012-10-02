5ee2cf72ff2949aeabe05a1546fd45ce6146b294
Anish Shankar <rndanish@gmail.com>
2012-10-02 12:40:47 +0530

    Linted

--- src/foreground.js
+++ src/foreground.js
@@ -1,35 +1,30 @@
 // Foreground helper functions this script will be loaded after jquery before popup.js
-function remNotif(uid,site)
-{
-    var new_notifications=localStorage.getItem('new_notifications');
-    if(new_notifications==null)
-	    return;
-    new_notifications=JSON.parse(new_notifications);
-    for(i=0;i<new_notifications.length;i++)
-    {
-	    if(new_notifications[i]["uid"]==uid&&new_notifications[i]["site"]==site)
-	    {
-		    new_notifications.splice(i,1);
-		    break;
-	    }
-    }
-    localStorage.setItem('new_notifications',JSON.stringify(new_notifications));
+function remNotif(uid, site) {
+	var new_notifications = localStorage.getItem('new_notifications');
+	if (new_notifications == null)
+		return;
+	new_notifications = JSON.parse(new_notifications);
+	for ( i = 0; i < new_notifications.length; i++) {
+		if (new_notifications[i]["uid"] == uid && new_notifications[i]["site"] == site) {
+			new_notifications.splice(i, 1);
+			break;
+		}
+	}
+	localStorage.setItem('new_notifications', JSON.stringify(new_notifications));
 }
-function remAll(site)
-{
-    var new_notifications=localStorage.getItem('new_notifications');
-    if(new_notifications==null)
-	    return;
-    new_notifications=JSON.parse(new_notifications);
-    for(i=0;i<new_notifications.length;i++)
-    {
-	    if(new_notifications[i]["site"]==site)
-	    {
-		    new_notifications.splice(i,1);
-		    break;
-	    }
-    }
-    localStorage.setItem('new_notifications',JSON.stringify(new_notifications));
+
+function remAll(site) {
+	var new_notifications = localStorage.getItem('new_notifications');
+	if (new_notifications == null)
+	new_notifications = JSON.parse(new_notifications);
+	for ( i = 0; i < new_notifications.length; i++) {
+		if (new_notifications[i]["site"] == site) {
+			new_notifications.splice(i, 1);
+			break;
+		}
+	}
+	localStorage.setItem('new_notifications', JSON.stringify(new_notifications));
 }
+
 //remNotif("12341234","courses");
 //remAll("facebook");
