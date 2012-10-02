86abff369c1cdebf6914a2afa59d4d22bd899a76
Anish Shankar <rndanish@gmail.com>
2012-10-02 11:25:48 +0530

    added gitignore (aptana compatible)

--- src/background.js
+++ src/background.js
@@ -1,2 +1,7 @@
 // This script will run on startup and will have  timer-loop running forever
 // Add background functions on top keep global constants on the very top of file
+
+
+function addHist(uid,site) {
+		
+}
